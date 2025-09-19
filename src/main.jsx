6b4ed85e7d4bd3c8dd5746b83c8877345c723779
Kamran Mushtaq <kaamipresents@gmail.com>
2025-09-20 00:46:07 +0500

    toast bg issue resolved

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,26 @@ import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.jsx'
 import { ThemeProvider } from './context/ThemeContext.jsx'
 import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <ThemeProvider>
-    <ToastContainer />
+    <ToastContainer
+      position="top-right"
+      autoClose={500}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="colored"
+      toastStyle={{ background: "var(--color-primary)", color: "var(--color-text)" }}
+    />
             <App />
         </ThemeProvider>
       </AuthProvider>
